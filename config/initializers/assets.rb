5fae15d2f568a430cecd66d4fae57413879835a1
yamashita <katsuji_yamashita@unirita.co.jp>
2016-04-10 03:51:12 +0900

    Chapter 6 ended.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,3 @@
 Rails.application.config.assets.precompile += %w( staff.css )
+Rails.application.config.assets.precompile += %w( admin.css )
+Rails.application.config.assets.precompile += %w( customer.css )
