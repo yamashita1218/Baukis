b987f41dd7bd6a57626109037be14cccc6c69c0b
yamashita <katsuji_yamashita@unirita.co.jp>
2016-04-10 02:07:09 +0900

    Added index.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -25,6 +25,12 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+  
+  config.order = "random"
+  config.include FactoryGirl::Syntax::Methods
+  config.before(:suite) do
+    FactoryGirl.reload
+  end
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
