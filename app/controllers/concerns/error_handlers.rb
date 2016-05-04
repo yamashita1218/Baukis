016b342614a72144009b8304ea4ea014305640a7
yamashita <katsuji_yamashita@unirita.co.jp>
2016-05-04 21:09:37 +0900

    Added edit account's information function.

--- app/controllers/concerns/error_handlers.rb
+++ app/controllers/concerns/error_handlers.rb
@@ -3,6 +3,7 @@ module ErrorHandlers
   
   included do
     rescue_from Exception, with: :rescue500
+    rescue_from ActionController::ParameterMissing, with: :rescue400
     rescue_from ApplicationController::Forbidden, with: :rescue403
     rescue_from ApplicationController::IpAddressRejected, with: :rescue403
     rescue_from ActionController::RoutingError, with: :rescue404
@@ -15,6 +16,11 @@ module ErrorHandlers
     render 'errors/internal_server_error', status: 500
   end
   
+  def rescue400(e)
+    @exception = e
+    render 'errors/bad_request', status: 400
+  end
+  
   def rescue403(e)
     @exception = e
     render 'errors/forbidden', status: 403
@@ -24,3 +30,4 @@ module ErrorHandlers
     @exception = e
     render 'errors/not_found', status: 404
   end
+end
