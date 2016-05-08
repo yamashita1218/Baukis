5d77ded546889e3d355001dd6e9ef0127a825752
yamashita <katsuji_yamashita@unirita.co.jp>
2016-05-09 00:16:31 +0900

    P.445

--- app/models/concerns/string_normalizer.rb
+++ app/models/concerns/string_normalizer.rb
@@ -15,4 +15,7 @@ module StringNormalizer
     NKF.nkf('-w -Z1 --katakana', text).strip if text
   end
   
+  def normalize_as_postal_code(text)
+    NKF.nkf('-w -Z1', text).strip.gsub(/-/, '') if text
+  end
 end
